d77a40bc4688772f0721e6483471253e29cf61e0
RainSnowHan <2652609017@qq.com>
2018-11-16 17:30:28 +0800

    hikirin: Enable HW codes to fix video play

--- releasetools/releasetools.hi6250.sh
+++ releasetools/releasetools.hi6250.sh
@@ -134,9 +134,8 @@ if [ "$(grep ro.build.version.release /vendor/build.prop)" = "ro.build.version.r
     # Use vndk 26
     sed -i "s/27/26/" /system/etc/init/gsi/init.vndk-27.rc
 
-    # Copy over vendor media_codecs.xml and disable unwanted HW codecs
+    # Copy over vendor media_codecs.xml
     cp /vendor/etc/media_codecs.xml /system/etc/media_codecs.xml
-    sed -i "s/<MediaCodec name=\"OMX.hisi.video.decoder.avc\" type=\"video\/avc\" >/<MediaCodec name=\"OMX.hisi.video.decoder.avc\" type=\"video\/no-avc\" >/g" /system/etc/media_codecs.xml
 fi
 
 exit 0
